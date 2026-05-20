b8c454afd591b569be916d06783975aa192da88e
KONIJETI VENKATA SESHU BABU <23B91A04A5@srkrec.ac.in>
2026-05-20 17:25:28 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -39,10 +39,8 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
-import streamlit as st
-if "google26af90abb4767635" in st.experimental_get_query_params() or st.query_params.get("page") == "google26af90abb4767635.html":
-    st.write("google-site-verification: google26af90abb4767635.html")
-    st.stop()
+import streamlit.components.v1 as components
+components.html('<meta name="google-site-verification" content="eyk5YWFpwTXE7mSnaRT-Uf18rMxtqh9zFOihRAF41Xw" />', height=0)
 # ─────────────────────────────────────────────────────────────────────────────
 # CUSTOM CSS
 # ─────────────────────────────────────────────────────────────────────────────
